6b6b7a7110aaef95e6ef918bfc112f11fc6f311b
abdul-baari-shafiquee <abdulbaarishafiquee@gmail.com>
2025-05-19 09:21:57 +0500

    Initial Commit

---
+++ Shortcuts.js
@@ -0,0 +1,7 @@
+const toggleBtn = document.getElementById("theme-toggle");
+const body = document.body;
+
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("light");
+  toggleBtn.textContent = body.classList.contains("light") ? "🌙" : "🌞";
+});
